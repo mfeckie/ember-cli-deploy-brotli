2a16818fac1795f987ce0ee19f2c7d4be4b87924
Martin Feckie <mfeckie@MuttonNet.lan>
2017-04-04 22:14:33 +0800

    normalizeEntityName

--- blueprints/ember-cli-deploy-brotli/index.js
+++ blueprints/ember-cli-deploy-brotli/index.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 module.exports = {
   description: '',
+  normalizeEntityName: function() {},
   afterInstall: function() {
      return this.addPackageToProject('iltorb');
   }
