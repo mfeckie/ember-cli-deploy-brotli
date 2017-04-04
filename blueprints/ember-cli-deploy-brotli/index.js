dfac75861a902873e547e23ef021ff6f085697ec
Martin Feckie <mfeckie@MuttonNet.lan>
2017-04-04 22:16:57 +0800

    Remove unecessary blueprint

--- blueprints/ember-cli-deploy-brotli/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-module.exports = {
-  description: '',
-  normalizeEntityName: function() {},
-  afterInstall: function() {
-     return this.addPackageToProject('iltorb');
-  }
-};
